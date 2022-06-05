52f050c7953e905ea233ca8bc9c9067ccfa8489b
Matt Silver <Mattsilver54@yahoo.com>
2022-06-05 14:21:12 -0700

    add heroku

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.static('public'));
 
 // HTML routes
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 // API routes for notes page
